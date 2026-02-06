92e5629654f0e85eed98a5f4deac12c55d80c3b4
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-02-06 11:05:38 +0100

    Update IfElseE.java
    Exercise completed

--- 04-control-structures/exercises/IfElseE.java
+++ 04-control-structures/exercises/IfElseE.java
@@ -46,13 +46,53 @@ public class IfElseE
 
         // -------------------- Student Exercises --------------------
         // 1. Check if x is even or odd.
+        int x = 5;
+        if (x % 2 == 0) 
+        {
+        System.out.println("The number " + x + " is even.");
+        } else 
+        {
+        System.out.println("The number " + x + " is odd.");
+        }
 
         // 2. Check age and print "Adult" or "Minor".
+        int age = 31;
+        if (age >= 18) {
+        System.out.println("Adult");
+        } else {
+        System.out.println("Minor");
+        }
 
         // 3. Assign grades based on score using if-else if-else.
+        int score = 87;
+        if (score >= 90) {
+        System.out.println("Grade: A");
+        } else if (score >= 80) {
+        System.out.println("Grade: B");
+        } else if (score >= 70) {
+        System.out.println("Grade: C");
+        } else if (score >= 60) {
+        System.out.println("Grade: D");
+        } else {
+        System.out.println("Grade: F");
+        }
 
         // 4. Check temperature and print "Hot", "Warm", or "Cold".
+        int temperature = 10;
+        if (temperature >= 30) {
+        System.out.println(" Weather is Hot");
+        } else if (temperature >= 20) {
+        System.out.println("Weather is Warm");
+        } else {
+        System.out.println("Weather is Cold");
+        }
 
         // 5. Check if n is positive AND even using logical operators.
+        int n = 12;
+        if (n > 0 && n % 2 == 0) {
+        System.out.println("The number " + n + " is positive AND even.");
+        } else {
+        System.out.println("The number " + n + " is NOT positive AND even.");
+        }
     }
 }
